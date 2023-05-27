4a9a3742d723699458314ce2e851c8370dddb797
jrag-dev <josealvarado2117@gmail.com>
2023-05-27 17:45:07 -0400

    solution to problem with git and src/pages

---
+++ src/pages/auth/LoginPage.jsx
@@ -0,0 +1,10 @@
+
+
+export function LoginPage () {
+  
+  return (
+    <div>
+      Login Pages
+    </div>
+  )
+}
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import { useContext } from 'react'
+import { ThemeContext } from '../../context/ThemeContext'
+import { HeaderComponent } from '../../components/HeaderComponent'
+import { FormComponent } from '../../components/FormComponent'
+import { ListTodoComponent } from '../../components/ListTodoComponent'
+import { useFiltersTodo } from '../../hooks/useFiltersTodo'
+import { useTodo } from '../../hooks/useTodo'
+import { useCurrentWidth } from '../../hooks/useCurrentWidth'
+
+import './HomePage.scss';
+
+export function HomePage () {
+	const { filters, filtersTodo, filteredTodos } = useFiltersTodo()
+	const { todos, createTodo, deleteTodo, completedTodoItem, clearTodoCompleted } = useTodo()
+	const { width } = useCurrentWidth()
+	const { theme } = useContext(ThemeContext)
+	
+	const breakPoints = {
+		mobile: 768,
+		table: 998
+	}
+	const todoLength = todos.length
+	const filterTodo = filteredTodos(todos)
+
+	return (
+		<section className="todo">
+			<HeaderComponent/>
+			<article className="todo-container">
+				<FormComponent
+					createTodo={createTodo}
+				/>
+				<ListTodoComponent
+					todos={filterTodo}
+					deleteTodo={deleteTodo}
+					completedTodoItem={completedTodoItem}
+				/>
+				
+				{
+					width <= breakPoints.mobile ? (
+					<>
+						<div className={`todo-clear ${!theme ? 'dark' : 'light'}`}>
+							<p>{todoLength} items left</p>
+							<button
+								onClick={clearTodoCompleted}
+							>Clear Completed</button>
+						</div>
+						<div className={`todo-actions ${!theme ? 'dark' : 'light'}`}>
+							<button onClick={() => filtersTodo({ all: true, active: false, completed: false})}>All</button>
+							<button onClick={() => filtersTodo({ all: false, active: true, completed: false})}>Active</button>
+							<button onClick={() => filtersTodo({ all: false, active: false, completed: true})}>Completed</button>
+						</div>
+					</>	
+					) : (
+						<div className={`todo-actions-all ${!theme ? 'dark' : 'light'}`}>
+							<p>{todoLength} items left</p>
+							<div className={`todo-actions-filters ${!theme ? 'dark' : 'light'}`}>
+								<button className={`${filters.all ? 'active' : ''}`} onClick={() => filtersTodo({ all: true, active: false, completed: false})}>All</button>
+								<button className={`${filters.active ? 'active' : ''}`} onClick={() => filtersTodo({ all: false, active: true, completed: false})}>Active</button>
+								<button className={`${filters.completed ? 'active' : ''}`} onClick={() => filtersTodo({ all: false, active: false, completed: true})}>Completed</button>
+							</div>
+							<button
+								onClick={clearTodoCompleted}
+							>Clear Completed</button>
+						</div>
+					)
+				}
+			</article>
+			
+			<p className="message-drag-drop">Drag and drop to recorder list</p>
+		</section>
+	)
+}
